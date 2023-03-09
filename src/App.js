01bdb5e6c6f2db4f1d5a4f75150bafe10ba4197d
Pedro Alex <pedroalex663@gmail.com>
2023-03-09 10:21:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [dados, setDados] = React.useState(null);
+
+  function random() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    fetch("https://icanhazdadjoke.com", config)
+      .then((response) => response.json())
+      .then((data) => setDados(data));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
+      <button style={{width:'120px', height:'50px', borderRadius:'15px', color:'#fff', background:'#450969', border:'none'}}  onClick={random}>Piada Aleatoria</button>
+      {dados && (
+        <div style={{width:'600px', height:'400px', background:'#450969', color:'#fff',marginTop:'10px', display:'flex', justifyContent:'center', alignItems:'center'}}>
+          <h1 style={{textAlign:'center'}}>{dados.joke}</h1>
+        </div>
+      )}
     </div>
   );
 }
